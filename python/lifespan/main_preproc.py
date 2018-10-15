8b2c54908f6c4816f61228d9ac405af5b8f246ba
Cosmo Zhang <zjzxz3006@126.com>
2018-10-15 23:41:00 +0800

    dev

--- python/lifespan/main_preproc.py
+++ python/lifespan/main_preproc.py
@@ -2,6 +2,7 @@ from . import mainparams as mp
 from .global_vars import global_vars as gv
 from .image_item import ImageItem
 import numpy as np, cv2, torch
+import skimage
 import os
 
 ims = gv["images"]
@@ -14,19 +15,26 @@ def torch_imopen(bw, stel):
     sumstel = torch.sum(stel)
     kh,kw = tuple(stel.shape[-2:])
     pdh,pdw = (kh/2,kw/2)
-    bw = (torch.conv2d(bw, stel, padding=(pdh,pdw)) == sumstel)
-    bw = (torch.conv2d(bw, stel, padding=(pdh,pdw)) > 0)
+    bw = (torch.conv2d(bw, stel, padding=(pdh,pdw)) == sumstel).type(torch.float32)
+    bw = (torch.conv2d(bw, stel, padding=(pdh,pdw)) > 0).type(torch.float32)
     return bw
-
-def torch_bwlabel(bw):
-    pass
 
 def detect_worm_2d(image):
     thv, bw = cv2.threshold(image, 0, 1, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    # imopen
-    stel = np.zeros([5,5]) + 1
-    bw = torch(bw).cuda()
+    # image open operation
+    bw = torch(bw).cuda().type(torch.float32)
     bw = torch_imopen(bw, np.zeros([5,5]) + 1)
+    # find max-area region
+    bw = bw.type(torch.uint8).cpu().numpy().reshape(list(bw.shape[-2:]))
+    bwl,nbwl = skimage.measure.label(bw, return_num=True)
+    bwp = skimage.measure.regionprops(bwl)
+    bwa = [x.area for x in bwp]
+    maxlabel, maxarea = (0, 0)
+    for i in range(nbwl):
+        if bwa[i] > maxarea:
+            maxarea = bwa[i]
+            maxlabel = i + 1
+    bw = (bwl == maxlabel)
 
 for i in range(mp.nfiles):
     ims[i].bw = ims[i].data
