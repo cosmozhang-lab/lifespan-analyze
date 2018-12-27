aba994ae661f364a8c2c1a7dad92083e10eb0445
Cosmo Zhang <cosmozhang1995@gmail.com>
2018-12-27 10:13:20 +0800

    dev

--- lifespan/learning/main.py
+++ lifespan/learning/main.py
@@ -14,7 +14,7 @@ def train():
     if userconfig.clear_model:
         Trainer.clear_model(model_path)
     load_model = model_path if userconfig.load_model else None
-    trainer = Trainer(datasetdir, generate_from=userconfig.datamark_storage, device=userconfig.device, load_model=load_model)
+    trainer = Trainer(datasetdir, generate_from=userconfig.datamark_storage, device=userconfig.device, load_model=load_model, dataset_info_path=userconfig.dataset_info_path)
     # trainer.dataset.check()
     save_model = model_path if userconfig.save_model else None
     trainer.train(with_test=True, log_steps=200, save_model=save_model)
