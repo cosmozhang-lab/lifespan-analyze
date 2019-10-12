bad38d09de9e87ab112f8313e96c788e8a4ad576
Cosmo Zhang <cosmozhang1995@gmail.com>
2019-10-12 12:46:09 +0800

    dev

--- matlab/params.example.m
+++ matlab/params.example.m
@@ -1,23 +1,29 @@
 % Where to find the result file and the images.
 % If you don't want to specify this option, set it as ''.
 rootdir = '/disk1/home/cosmo/downloads/lifespan';
+
 % Result MAT filename.
 % Final result filename will be formed as
 % <rootdir>/<resultfile>
 resultfile = 'G7.out.mat';
+
 % Image filenames' prefix and suffix.
 % Final image filename will be formed as
 % <rootdir>/<prefix><datetime><suffix>.
 imgprefix = 'G4\\G4__';
 imgsuffix = '.tiff';
+
 % Frames per day
 framerate = 24;
+
 % Image scaling factor.
 % If you use resized images instead of the original
 % images, you must specify it here. Otherwise leave
 % it as 1.
 sc = 1;
+
 % Frames per day
 framerate = 24;
+
 % Maximum number of files to watch.
 maxnfiles = Inf;
